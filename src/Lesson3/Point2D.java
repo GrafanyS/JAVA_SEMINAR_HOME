4a56168397efb59040b173176299a36f7974197a
GrafanyS <admin@valet.of.by>
2022-10-25 11:48:33 +0300

    точка 2D

--- src/Lesson3/Point2D.java
+++ src/Lesson3/Point2D.java
@@ -1,4 +1,23 @@
 package Lesson3;
 
 public class Point2D {
+    int x, y;
+
+    public Point2D(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("x: %d  y: %d", x, y);
+    }
 }
