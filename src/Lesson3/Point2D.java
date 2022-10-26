30c2ff303de5ffcaa17be7ffcd07f0fb2d98cffd
GrafanyS <admin@valet.of.by>
2022-10-26 08:56:55 +0300

    Точка в пространстве 2D

--- src/Lesson3/Point2D.java
+++ src/Lesson3/Point2D.java
@@ -1,19 +1,11 @@
 package Lesson3;
 
-public class Point {
+public class Point2D {
     int x, y;
 
-    public Point(int x, int y) {
+    public Point2D(int x, int y) {
         this.x = x;
         this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
     }
 
     @Override
